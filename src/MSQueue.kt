394a3be7cfc45ca8485394a2040be027a0b693b8
Denis Shabanov <shadenis2015@gmail.com>
2023-09-14 18:52:04 +0200

    Implement non-blocking Michael-Scott concurrent queue

--- src/MSQueue.kt
+++ src/MSQueue.kt
@@ -1,15 +1,52 @@
 import java.util.concurrent.atomic.AtomicReference
 
 class MSQueue<E> : Queue<E> {
-    private val head: AtomicReference<Node<E>>
-    private val tail: AtomicReference<Node<E>>
+    private val dummyNode = Node<E>(null)
+    private val head: AtomicReference<Node<E>> = AtomicReference(dummyNode)
+    private val tail: AtomicReference<Node<E>> = AtomicReference(dummyNode)
 
     override fun enqueue(element: E) {
-        TODO("Implement me")
+        val newNode = Node(element)
+
+        var tailNode: Node<E>
+        while (true) {
+            tailNode = tail.get()
+            val nextNode = tailNode.next.get()
+            if (tailNode === tail.get()) {
+                if (nextNode == null) {
+                    if (tailNode.next.compareAndSet(nextNode, newNode)) {
+                        break
+                    }
+                } else {
+                    tail.compareAndSet(tailNode, nextNode)
+                }
+            }
+        }
+        tail.compareAndSet(tailNode, newNode)
     }
 
     override fun dequeue(): E? {
-        TODO("implement me")
+        var result: E?
+        while (true) {
+            val headNode = head.get()
+            val tailNode = tail.get()
+            val nextNode = headNode.next.get()
+            if (headNode === head.get()) {
+                if (headNode === tailNode) {
+                    if (nextNode == null) {
+                        return null
+                    }
+                    tail.compareAndSet(tailNode, nextNode)
+                } else {
+                    result = nextNode?.element
+                    if (head.compareAndSet(headNode, nextNode)) {
+                        break
+                    }
+                }
+            }
+        }
+        head.get().element = null
+        return result
     }
 
     // FOR TEST PURPOSE, DO NOT CHANGE IT.
